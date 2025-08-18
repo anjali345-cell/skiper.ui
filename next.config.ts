7be37f77b1f3390f06debdf5637bcf8e7895d7d5
anjali345-cell <as0172085@gmail.com>
2025-08-18 13:38:05 +0530

    removed number

--- next.config.ts
+++ next.config.ts
@@ -3,5 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 };
+module.exports = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+
+}
+
 
 export default nextConfig;
--- src/components/homeCards/EndCard.tsx
+++ src/components/homeCards/EndCard.tsx
@@ -7,34 +7,47 @@ const EndCard = () => {
       <div className=" relative mx-auto  h-[400px] max-w-4xl overflow-hidden rounded-[38px] border border-black/5 bg-[#ff4017] p-2 shadow-sm ">
         <p className="mt-20 text-center text-white">Bringing simplicity to the future</p>
 
-           <div className="flex justify-center w-full">
-        <a href="#" className="items-center mt-10 rounded-full border border-white/20 text-sm p-1 font-medium">
-          <p
-            style={{ 'shiny-width': '100px' }}
-            className="mx-auto max-w-md dark:text-neutral-400/70 animate-shiny-text bg-clip-text bg-no-repeat [background-position:0_0] [background-size:var(--shiny-width)_100%] [transition:background-position_1s_cubic-bezier(.6,.6,0,1)_infinite] bg-gradient-to-r from-transparent via-black/80 via-50% to-transparent dark:via-white/80 inline-flex group text-white items-center border justify-center rounded-full px-4 py-1 transition ease-out"
-          >
-            <span> 🎉 &nbsp;| New component in </span> &nbsp;
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="lucide lucide-arrow-right ml-1 size-3 group-hover:rotate-180 group-hover:ml-4 transition-all duration-300 ease-in-out group-hover:translate-x-0.5"
+        <div className="flex justify-center w-full leading-none">
+          <a href="#" className="items-center mx-auto mt-10 rounded-full border border-white/20 text-sm p-1 font-medium">
+            <span
+              style={{ 'maxWidth': '600px' }}
+              className="mx-auto max-w-md dark:text-neutral-400/70 animate-shiny-text bg-clip-text bg-no-repeat [background-position:0_0] [background-size:var(--shiny-width)_100%] [transition:background-position_1s_cubic-bezier(.6,.6,0,1)_infinite] bg-gradient-to-r from-transparent via-black/80 via-50% to-transparent dark:via-white/80 inline-flex group text-white items-center border justify-center rounded-full px-4 py-1 transition ease-out"
             >
-              <path d="M5 12h14"></path>
-              <path d="m12 5 7 7-7 7"></path>
-            </svg>
-          </p>
-        </a>
+
+              <span> 🎉 &nbsp;| New component in </span> &nbsp;
+              <span className="scale-50 inline-flex text-[10px] leading-none px-1">
+
+                <AnimatedNumberCountdown
+                  endDate={new Date("2025-12-31")}
+                />
+              </span>
+
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="lucide lucide-arrow-right ml-1 size-3 group-hover:rotate-180 group-hover:ml-4 transition-all duration-300 ease-in-out group-hover:translate-x-0.5"
+              >
+                <path d="M5 12h14"></path>
+                <path d="m12 5 7 7-7 7"></path>
+              </svg>
+
+
+            </span>
+          </a>
+        </div>
+        <h1 className="mt-[0] text-[150px] flex justify-center border-none opacity-85">
+
+          𝕊𝕜𝕚𝕡𝕖𝕣/𝕦𝕚</h1>
+
       </div>
-      <h1 className="absolute inset-x-0 mt-[80px] text-center text-[100px] font-semibold text-[#ff4017] sm:mt-[30px] sm:text-[190px] ">skiper/ui </h1>
-      </div>
-   
+
     </div>
   )
 }
